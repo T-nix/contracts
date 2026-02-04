72a3c14286d61dc59f69900b137ecde93833d49c
mikitali <mykhaylotal@gmail.com>
2026-02-04 21:51:13 +0200

    Add proto permissions

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
 
 export const PROTO_PATHS = {
-    AUTH: join(__dirname, '../../proto/auth.proto')
+    AUTH: join(__dirname, '../../proto/auth.proto'),
+    PERMISSION: join(__dirname, '../../proto/permission.proto')
 } as const
