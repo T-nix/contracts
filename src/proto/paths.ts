095713b8124ccbc89756213271ad02e979587cda
mikitali <mykhaylotal@gmail.com>
2026-02-21 13:42:16 +0200

    fix: config settings

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -33,7 +33,7 @@ export function  getServiceConfig(serviceName: ProtoKey, config: ConfigService):
     const host = config.get<string>(`grpc.${serviceName}.host}`, {infer: true}) || PROTO_PATHS[serviceName].host
     const port = config.get<string>(`grpc.${serviceName}.port}`, {infer: true}) || PROTO_PATHS[serviceName].port
     const file = config.get<string>(`grpc.${serviceName}.file}`, {infer: true}) || PROTO_PATHS[serviceName].file
-    const packageVersion = config.getOrThrow<string>(`grpc.${serviceName}.version}`, {infer: true}) || PROTO_PATHS[serviceName].version
+    const packageVersion = config.get<string>(`grpc.${serviceName}.version}`, {infer: true}) || PROTO_PATHS[serviceName].version
     return {
         file,
         url: `${host}:${port}`,
