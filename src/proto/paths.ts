a6e292f63ff68f70f8d4f2f36c2e4691ddf5b825
Michael <mykhaylotal@gmail.com>
2026-02-21 10:40:56 +0200

    Add new user proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -2,5 +2,6 @@ import { join } from "path";
 
 export const PROTO_PATHS = {
     AUTH: join(__dirname, '../../proto/auth.proto'),
-    PERMISSION: join(__dirname, '../../proto/permission.proto')
+    PERMISSION: join(__dirname, '../../proto/permission.proto'),
+    USER: join(__dirname, '../../proto/user.proto')
 } as const
