d15580f18c6fd0535226fd5276dbd0017e82b5e9
mikitali <mykhaylotal@gmail.com>
2026-02-28 21:49:42 +0200

    Fix contracts

--- src/clients/grpc.module.ts
+++ src/clients/grpc.module.ts
@@ -1,12 +1,10 @@
 import { type DynamicModule, Module } from "@nestjs/common";
 import { GrpcClientFactory } from "./factory/grpc.client.factory";
-import { GRPC_CLIENT_PREFIX } from "./constants/grpc.const";
 import { ConfigService } from "@nestjs/config";
 import { getServiceConfig, ProtoKey } from "../proto";
-import * as Services from './services'
+
 import { GrpcClientsService } from "./services/clients.service";
 
-const providers = Object.values(Services);
 @Module({})
 export class GrpcModule {
     public static register(clients: ProtoKey[]): DynamicModule {
@@ -14,28 +12,6 @@ export class GrpcModule {
             module: GrpcModule,
             providers: [
                 GrpcClientFactory,
-
-                // register each client one by one
-                /*
-                ...clients.map((token) => {
-                    return {
-                        provide: `${GRPC_CLIENT_PREFIX}_${token}`,
-                        useFactory: (factory: GrpcClientFactory, config: ConfigService) => {
-                            const cnf = getServiceConfig(token, config)
-
-                            const client = factory.createClient({
-                                package: cnf.packageVersion,
-                                protoPath: cnf.file,
-                                url: cnf.url
-                            })
-                            factory.register(token, client)
-
-                            return client
-                        },
-                        inject: [GrpcClientFactory, ConfigService]
-                    }
-                })
-                    */
                 {
                     provide: 'GRPC_CONFIG',
                     useValue: clients,
@@ -62,7 +38,7 @@ export class GrpcModule {
                 },
                GrpcClientsService
             ],
-            exports: [GrpcClientFactory, ...clients.map(token => `${GRPC_CLIENT_PREFIX}_${token}`)]
+            exports: [GrpcClientFactory, GrpcClientsService]
         }
     }
 }
